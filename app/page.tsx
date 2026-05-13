8162ea7324d648f9c3e0d4dec4e4cf8029b3566e
Ismail Hossain <mdismailtanvir6@gmail.com>
2026-05-13 09:29:58 +0600

    again testing commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
       Hello next world!, this is a next demo project
+      And this is master branch
     </div>
   );
 }
